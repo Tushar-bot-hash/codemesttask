86461b8e0801f2ed04582ad85a2950feacc4b37d
Tushar-bot-hash <tusharv811@gmail.com>
2026-03-14 18:03:04 +0530

    deploy

--- backend/server.js
+++ backend/server.js
@@ -6,11 +6,16 @@ import notesRouter from './routes/notes.js'
 dotenv.config()
 
 const app = express()
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 10000
 
-app.use(cors())
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://codemesttask.vercel.app'
+  ]
+}))
+
 app.use(express.json())
-
 app.use('/notes', notesRouter)
 
 app.get('/', (req, res) => {
